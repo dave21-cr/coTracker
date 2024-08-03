1eb360d9f88e371883d7ad40c1e6a99afb9e5c75
fobybus <62669476+fobybus@users.noreply.github.com>
2024-08-03 17:17:34 +0300

    Add tasklist page

--- app/(internal)/task/page.tsx
+++ app/(internal)/task/page.tsx
@@ -1,11 +1,15 @@
-//tasks list page
-import React from "react"
-export default function TaskInfo(){
+import Tablebox from "@/app/components/table"
+import { Button } from "@/components/ui/button"
 
+//tasks list page
+export default function TaskInfo() {
     return (
-    <h1>
-        task list  
-    </h1>
-    ) 
+        <div className="container">
+            <h1 className="text-5xl font-bold max-sm:text-3xl text-slate-900 dark:text-white">Tasks</h1>
+            <div className="flex flex-row justify-start">
+                <Button variant={"outline"} className="text-white font-bold hover:bg-green-500 bg-green-800">New</Button>
+            </div>
+            <Tablebox />
+        </div>
+    )
 }
-        
